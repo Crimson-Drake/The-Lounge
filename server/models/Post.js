9223cec51596cff447e14e2bea104d86626c89a7
vinsdragonis <zrexteam128@gmail.com>
2021-11-28 21:06:11 +0530

    Added postScehma

---
+++ server/models/Post.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    UserId: {
+        type: String,
+        required: true
+    },
+    desc: {
+        type: String,
+        max: 500
+    },
+    img: {
+        type: String
+    },
+    likes: {
+        type: Array,
+        default: []
+    }
+},
+{
+    timestamps: true
+}
+);
+
+module.exports = mongoose.model('Post', postSchema);
