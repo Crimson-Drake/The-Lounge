f67893a5dba3cec2222dc76c5edfaee3ba97580d
vinsdragonis <zrexteam128@gmail.com>
2021-11-28 20:00:25 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,6 +16,11 @@ mongoose.connect(process.env.MONGO_URL,
     console.log("Connected to MongoDB");
 });
 
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
 app.listen(3001, () => {
   console.log("Backend server is running!");
 });
