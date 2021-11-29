fecf7c3255cd6ffdb98f945a46c3040ca8b6d12e
vinsdragonis <zrexteam128@gmail.com>
2021-11-29 21:49:32 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,15 +2,19 @@ const router = require('express').Router();
 const User = require('../models/User');
 
 // REGISTER
-router.get("/register", async (req, res) => {
-    const user = await new User({
-        username: "dragonis",
-        email: "dragonis@gmail.com",
-        password: "dragonRoar"
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
     });
 
-    await user.save();
-    res.send("Saved");
+    try {
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch (err) {
+        console.error(err);
+    }
 });
 
 module.exports = router;
