51bf641123723e1113acfe5bc8475be19fb8eab0
vinsdragonis <zrexteam128@gmail.com>
2021-11-29 23:15:39 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,15 +1,19 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 // REGISTER
 router.post("/register", async (req, res) => {
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    });
-
     try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword
+        });
+
         const user = await newUser.save();
         res.status(200).json(user);
     } catch (err) {
