14abd229a65e78cc12c15a03d35ea342d97d3d88
vinsdragonis <zrexteam128@gmail.com>
2021-11-30 00:29:04 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,7 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 
 // REGISTER
+
 router.post("/register", async (req, res) => {
     try {
         const salt = await bcrypt.genSalt(10);
@@ -18,6 +19,28 @@ router.post("/register", async (req, res) => {
         res.status(200).json(user);
     } catch (err) {
         console.error(err);
+    }
+});
+
+// LOGIN
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+      
+        if (!user) {
+            res.status(404).json("user not found");
+        } else {
+            const validPassword = await bcrypt.compare(req.body.password, user.password);
+        
+            if (!validPassword) {
+                res.status(400).json("wrong password");
+            } else {
+                res.status(200).json(user);
+            }
+        }
+    } catch (err) {
+      res.status(500).json(err)
     }
 });
 
