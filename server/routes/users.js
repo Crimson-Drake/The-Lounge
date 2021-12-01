dec68c05c43060ea131f1e06184b7e0d7e20403e
vinsdragonis <zrexteam128@gmail.com>
2021-12-01 18:30:12 +0530

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -46,6 +46,16 @@ router.delete(':/id', async function(req, res) {
 });
 
 // get a user
+router.get(':/id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, updatedAt, ...other } = user._doc;
+        res.status(200).json(other);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 // follow users
 // unfollow users
 
