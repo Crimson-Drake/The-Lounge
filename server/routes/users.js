77b3fbfe0d896a89187f2c38a6894f2f622e85b4
vinsdragonis <zrexteam128@gmail.com>
2021-12-01 18:26:05 +0530

    Added delete user route

--- server/routes/users.js
+++ server/routes/users.js
@@ -30,7 +30,21 @@ router.put("/:id", async (req, res) => {
         return res.status(403).json("You can't update this user");
     }
 });
+
 // delete user
+router.delete(':/id', async function(req, res) {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        try {
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("Account has been deleted");
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("You can delete only your own account");
+    }
+});
+
 // get a user
 // follow users
 // unfollow users
