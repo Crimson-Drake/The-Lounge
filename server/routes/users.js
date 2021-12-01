2eda966552ab7a5771b0941db064d9670bd2532d
vinsdragonis <zrexteam128@gmail.com>
2021-12-01 18:11:29 +0530

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 router.get("/", (req, res) => {
     res.send("Reached users page");
