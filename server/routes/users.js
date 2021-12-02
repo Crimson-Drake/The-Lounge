70f8c9c33bc7889d948e3ee1019562206d261b24
vinsdragonis <zrexteam128@gmail.com>
2021-12-02 07:19:22 +0530

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -46,7 +46,7 @@ router.delete('/:id', async (req, res) => {
 });
 
 // get a user
-router.get(':/id', async (req, res) => {
+router.get('/:id', async (req, res) => {
     try {
         const user = await User.findById(req.params.id);
         const { password, updatedAt, ...other } = user._doc;
