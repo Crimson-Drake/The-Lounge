e1de9f3dc5edafcd9e8d0d0751a9772844a491dc
vinsdragonis <zrexteam128@gmail.com>
2021-12-03 02:13:14 +0530

    Update posts.js

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -73,6 +73,18 @@ router.get("/:id", async (req, res) => {
 });
 
 // Get timeline posts
-
+router.get("/timeline/all", async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({ userId: currentUser._id });
+        const friendPosts = await Promise.all(
+            currentUser.following.map(friendId => {
+                return Post.find({ userId: friendId });
+            })
+        )
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
