9278dd80f9276efb06d90bb3d99f1f6453b58f7b
vinsdragonis <zrexteam128@gmail.com>
2021-12-05 02:06:24 +0530

    .

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,12 @@
 import './sidebar.css';
-import { RssFeed } from '@material-ui/icons';
+import {
+    RssFeed,
+    Chat,
+    Group,
+    Bookmark,
+    HelpOutline,
+    Event,
+} from '@material-ui/icons';
 
 export default function Sidebar() {
     return (
@@ -7,8 +14,28 @@ export default function Sidebar() {
             <div className="sidebarWrapper">
                 <ul className="sidebarList">
                     <li className="sidebarListItem">
-                        <RssFeed />
+                        <RssFeed className="sidebarIcon" />
                         <span className="sidebarListItemText">Feed</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Chat className="sidebarIcon" />
+                        <span className="sidebarListItemText">Chats</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Group className="sidebarIcon" />
+                        <span className="sidebarListItemText">Groups</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Bookmark className="sidebarIcon" />
+                        <span className="sidebarListItemText">Bookmarks</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <HelpOutline className="sidebarIcon" />
+                        <span className="sidebarListItemText">Questions</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Event className="sidebarIcon" />
+                        <span className="sidebarListItemText">Events</span>
                     </li>
                 </ul>
             </div>
