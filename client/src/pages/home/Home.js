e70ac4dbe6faa2cedddf937bde181bb0589e2e16
vinsdragonis <zrexteam128@gmail.com>
2021-12-05 01:30:26 +0530

    Update Home.js

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,11 +1,17 @@
 import Topbar from '../../components/topbar/Topbar';
 import Sidebar from '../../components/sidebar/Sidebar';
+import Feed from '../../components/feed/Feed';
+import Rightbar from '../../components/rightbar/Rightbar';
 
 export default function Home() {
     return (
-        <div>
+        <>
             <Topbar />
-            <Sidebar />
-        </div>
+            <div className="homeContainer">
+                <Sidebar />
+                <Feed />
+                <Rightbar />
+            </div>
+        </>
     );
 }
