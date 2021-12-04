4bbb064b66329a71e3f1515fb26d3e31fb803d59
vinsdragonis <zrexteam128@gmail.com>
2021-12-04 19:40:49 +0530

    Built topbar

--- client/src/components/topbar/Topbar.js
+++
@@ -1,9 +0,0 @@
-import './topbar.css';
-
-export default function Topbar() {
-    return (
-        <>
-            This is the topbar
-        </>
-    );
-}
---
+++ client/src/components/topbar/Topbar.jsx
@@ -0,0 +1,41 @@
+import './topbar.css';
+import { Search, Person, Chat, Notifications } from "@material-ui/icons";
+
+export default function Topbar() {
+    return (
+        <div className="topbarContainer">
+            <div className="topbarLeft">
+                <span className="logo">The Lounge</span>
+            </div>
+            <div className="topbarCenter">
+                <div className="searchBar">
+                    <Search />
+                    <input
+                        placeholder="Search"
+                        className="searchInput"
+                    />
+                </div>
+            </div>
+            <div className="topbarRight">
+                <div className="topbarLinks">
+                    <span className="topbarLink">Home</span>
+                    <span className="topbarLink">Timeline</span>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbarIconItems">
+                        <Person />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItems">
+                        <Person />
+                        <span className="topbarIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconItems">
+                        <Person />
+                        <span className="topbarIconBadge">3</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
