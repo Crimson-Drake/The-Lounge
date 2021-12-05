975692894c3fc612c7981a06885bb24c1027bcbd
vinsdragonis <zrexteam128@gmail.com>
2021-12-05 20:55:37 +0530

    Created share component

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,9 +1,15 @@
 import './feed.css';
+import Share from '../share/Share';
 
 export default function Feed() {
     return (
         <div className="feed">
-            Feed
+            <div className="feedWrapper">
+                <Share />
+                {/* { Posts.map((p) => (
+                    <Post key={p.id} post={p} />
+                )) } */}
+            </div>
         </div>
     );
 }
---
+++ client/src/components/share/Share.jsx
@@ -0,0 +1,11 @@
+import './share.css';
+
+export default function Share() {
+    return (
+        <div className="share">
+            <div className="shareWrapper">
+                <div className="shareTop">Share</div>
+            </div>
+        </div>
+    );
+}
