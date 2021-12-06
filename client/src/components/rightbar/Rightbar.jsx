5fe2091403b786dc99da1de2378649346932c17f
vinsdragonis <zrexteam128@gmail.com>
2021-12-06 20:23:13 +0530

    Basic mod for rightbar

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -6,6 +6,10 @@ export default function Rightbar() {
         <div className="rightbar">
             <div className="rightbarWrapper">
                 {/* {profile ? <ProfileRightbar /> : <HomeRightbar />} */}
+                <div className="birthdayContainer">
+                    <img className="birthdayImg" src="/assets/person/dp.jpg" alt="" />
+                    <span className="birthdayText">King Dragonis and 5 others have a birthday today</span>
+                </div>
             </div>
         </div>
     );
