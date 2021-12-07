3d2d1d6d4f4bc0b2c57ce739a5d4bc058d16495b
vinsdragonis <zrexteam128@gmail.com>
2021-12-08 01:08:04 +0530

    Changed scrollbar color and styling

--- client/src/components/closeFriend/CloseFriend.jsx
+++ client/src/components/closeFriend/CloseFriend.jsx
@@ -1,6 +1,6 @@
 import "./closeFriend.css";
 
-export default function CloseFriend({user}) {
+export default function CloseFriend({ user }) {
     return (
         <li className="sidebarFriend">
             <img className="sidebarFriendImg" src={ user.profilePicture } alt="" />
