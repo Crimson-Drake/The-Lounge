96c48f2d8472b7333cd135e45fa94cc54a654bb0
vinsdragonis <zrexteam128@gmail.com>
2021-12-07 07:04:12 +0530

    Update Post.jsx

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -22,26 +22,26 @@ export default function Post({ post }) {
                         alt=""
                     />
                     <span className="postUsername">
-                    {Users.filter((u) => u.id === post?.userId)[0].username}
+                    { Users.filter((u) => u.id === post?.userId)[0].username }
                     </span>
-                    <span className="postDate">{post.date}</span>
+                    <span className="postDate">{ post.date }</span>
                 </div>
                 <div className="postTopRight">
                     <MoreVert />
                 </div>
             </div>
             <div className="postCenter">
-                <span className="postText">{post?.desc}</span>
-                <img className="postImg" src={post.photo} alt="" />
+                <span className="postText">{ post?.desc }</span>
+                <img className="postImg" src={ post.photo } alt="" />
             </div>
             <div className="postBottom">
                 <div className="postBottomLeft">
-                    <img className="likeIcon" src="assets/like.png" onClick={likeHandler} alt="" />
-                    <img className="likeIcon" src="assets/heart.png" onClick={likeHandler} alt="" />
-                    <span className="postLikeCounter">{like} people like it</span>
+                    <img className="likeIcon" src="assets/like.png" onClick={ likeHandler } alt="" />
+                    <img className="likeIcon" src="assets/heart.png" onClick={ likeHandler } alt="" />
+                    <span className="postLikeCounter">{ like } people like it</span>
                 </div>
                 <div className="postBottomRight">
-                    <span className="postCommentText">{post.comment} comments</span>
+                    <span className="postCommentText">{ post.comment } comments</span>
                 </div>
             </div>
         </div>
