ef3ae4cf4dacbf64b7f619468e5277aa59d05406
vinsdragonis <zrexteam128@gmail.com>
2021-12-08 00:59:58 +0530

    Update CloseFriend.jsx

--- client/src/components/closeFriend/CloseFriend.jsx
+++ client/src/components/closeFriend/CloseFriend.jsx
@@ -3,8 +3,8 @@ import "./closeFriend.css";
 export default function CloseFriend({user}) {
     return (
         <li className="sidebarFriend">
-            <img className="sidebarFriendImg" src={user.profilePicture} alt="" />
-            <span className="sidebarFriendName">{user.username}</span>
+            <img className="sidebarFriendImg" src={ user.profilePicture } alt="" />
+            <span className="sidebarFriendName">{ user.username }</span>
         </li>
     );
 }
