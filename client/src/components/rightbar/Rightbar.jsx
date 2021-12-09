f1b8e6794c12586245299b6a85847a6a5a1acb2d
vinsdragonis <zrexteam128@gmail.com>
2021-12-10 00:28:51 +0530

    Modded rightbar for profile page

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -2,10 +2,10 @@ import './rightbar.css';
 import { Users } from "../../dummyData";
 import Online from "../online/Online";
 
-export default function Rightbar() {
-    return (
-        <div className="rightbar">
-            <div className="rightbarWrapper">
+export default function Rightbar({ profile }) {
+    const HomeRightbar = () => {
+        return (
+            <>
                 <div className="birthdayContainer">
                     <img className="birthdayImg" src="/assets/gift.png" alt="" />
                     <span className="birthdayText">
@@ -23,6 +23,103 @@ export default function Rightbar() {
                         <Online key={ u.id } user={ u } />
                     ))}
                 </ul>
+            </>
+        );
+    }
+
+    const ProfileRightbar  = () => {
+        return (
+            <>
+                <h4 className="rightbarTitle">User information</h4>
+                <div className="rightbarInfo">
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">City:</span>
+                        <span className="rightbarInfoValue">Bangalore</span>
+                    </div>
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">From:</span>
+                        <span className="rightbarInfoValue">Los Angeles</span>
+                    </div>
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">Occupation:</span>
+                        <span className="rightbarInfoValue">Web Developer</span>
+                    </div>
+                </div>
+                <h4 className="rightbarTitle">User friends</h4>
+                <div className="rightbarFollowings">
+                    <div className="rightbarFollowing">
+                        <img
+                        src="assets/person/dp.jpg"
+                        alt=""
+                        className="rightbarFollowingImg"
+                        />
+                        <span className="rightbarFollowingName">King Dragonis</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img
+                        src="assets/person/dp.jpg"
+                        alt=""
+                        className="rightbarFollowingImg"
+                        />
+                        <span className="rightbarFollowingName">Vineeth</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img
+                        src="assets/person/dp.jpg"
+                        alt=""
+                        className="rightbarFollowingImg"
+                        />
+                        <span className="rightbarFollowingName">Vulnahkriin</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img
+                        src="assets/person/dp.jpg"
+                        alt=""
+                        className="rightbarFollowingImg"
+                        />
+                        <span className="rightbarFollowingName">Vins</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img
+                        src="assets/person/dp.jpg"
+                        alt=""
+                        className="rightbarFollowingImg"
+                        />
+                        <span className="rightbarFollowingName">Vineeth B V</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img
+                        src="assets/person/dp.jpg"
+                        alt=""
+                        className="rightbarFollowingImg"
+                        />
+                        <span className="rightbarFollowingName">Drago</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img
+                        src="assets/person/dp.jpg"
+                        alt=""
+                        className="rightbarFollowingImg"
+                        />
+                        <span className="rightbarFollowingName">John Carter</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img
+                        src="assets/person/dp.jpg"
+                        alt=""
+                        className="rightbarFollowingImg"
+                        />
+                        <span className="rightbarFollowingName">Tom Cruise</span>
+                    </div>
+                </div>
+            </>
+        );
+    }
+
+    return (
+        <div className="rightbar">
+            <div className="rightbarWrapper">
+                { profile ? <ProfileRightbar /> : <HomeRightbar /> }
             </div>
         </div>
     );
