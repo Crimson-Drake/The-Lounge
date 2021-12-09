4aa58f0bd2dad89b245c9f0ca8f924ad29666d5c
vinsdragonis <zrexteam128@gmail.com>
2021-12-10 02:36:56 +0530

    Shifted default page from login to home

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import Home from './pages/home/Home';
 import Profile from './pages/profile/Profile';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <div>
-      {/* <Home /> */}
-      <Profile />
+      <Home />
+      {/* <Profile /> */}
+      {/* <Login /> */}
     </div>
   );
 }
